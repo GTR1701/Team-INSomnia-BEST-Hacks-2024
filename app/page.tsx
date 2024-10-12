c39ad9c50c58ab4a48f2404748c20ee21cc61fa9
GTR1701 <dawidkowal04@gmail.com>
2024-10-12 13:34:02 +0200

    setup done

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Button variant="nav" className="transition">
+      <Button className="transition">
         Shadcn Button
       </Button>
     </>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,7 +19,6 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        nav: "w-fit h-fit bg-purple-500 text-3xl font-normal hover:font-bold transition hover:bg-purple-800"
       },
       size: {
         default: "h-9 px-4 py-2",
